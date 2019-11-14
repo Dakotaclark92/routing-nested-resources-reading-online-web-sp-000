b5d283ed44594e573515e52b08c473ea3c840f56
Dakota Clark <dakotaclark92@yahoo.com>
2019-11-14 01:56:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'authors/:id/posts'
-  get 'authors/:id/posts/:post_id'
+  get 'authors#posts_index'
+  get 'authors#post'
   
   resources :authors, only: [:show]
 
